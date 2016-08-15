3d982a41b52878e411067b121629deae44e34f8a
shaikheram <shaikh.eram74@gmail.com>
2016-08-15 20:48:06 +0530

    Add files via upload

--- re.java
+++ re.java
@@ -9,3 +9,15 @@ while((n = dis.read(buf)) != -1 && n!=3 ){
         }
             fos.close();
         }
+//NOw for encryption
+for(int i = 0; i < files.size();i++){
+    System.out.println("Receiving file: " + files.get(i).getName());
+ //create a new fileoutputstream for each new file
+fos = new FileOutputStream("C:\\eram\\encryt.c\\" +files.get(i).getName());
+//read file
+while((n = dis.read(buf)) != -1 && n!=3 ){
+        fos.write(buf,0,n);
+        fos.flush();
+        }
+            fos.close();
+        }
